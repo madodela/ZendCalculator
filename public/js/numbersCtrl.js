4686250d42805f5490d381d86bc59a399fbf0cff
Loli Delgado <madodela@gmail.com>
2014-03-23 19:13:49 -0600

    Funcionalidad de botones de numeros y operaciones continuas y mezcladas terminada

--- public/js/numbersCtrl.js
+++ public/js/numbersCtrl.js
@@ -1,96 +1,37 @@
 $(function() {
 
     var n1, n2, startDivision = true,
-            inSum = false, inDiv = false, equal = false;
+            inSum = false, inDiv = false, equal = false,
+            enterSomeNumbers = false;
+
     $.session.set('value', '0');
     $('#number').val(0);
 
     $('#addBtn').click(function() {
+        inSum = true;
         if (!equal) {
-            inSum = true;
-            n1 = parseFloat($.session.get('value'));
-            n2 = parseFloat($('#number').val());
-            $.session.set('value', add(n1, n2));
-            //input = session
-            $('#number').val($.session.get('value'));
-
+            if (inDiv) {
+                var tot = division(parseFloat($.session.get('value')),
+                        parseFloat($('#number').val()));
+                alert(tot);
+                cleanInput();
+                $('#number').val(tot);
+                $.session.set('value', tot);
+                inDiv = false;
+            } else if (inSum) {
+                n1 = parseFloat($.session.get('value'));
+                n2 = parseFloat($('#number').val());
+                $.session.set('value', add(n1, n2));
+                //input = session
+                $('#number').val($.session.get('value'));
+            }
         } else {
             //input = session = value a add
             equal = false;
             inSum = true;
         }
-
+        enterSomeNumbers = true;
     });
-
-    var add = function(a, b) {
-        return a + b;
-
-    };
-
-    var division = function(a, b) {
-        return a / b;
-    };
-
-    $('#cleanLocal').click(function() {
-        $('#number').val('0');
-        $.session.set('value', 0);
-        startDivision = true, inSum = false,
-                inDiv = false, equal = false;
-
-    });
-
-    var addNumber = function(n) {
-      var val = $('#number').val();
-      if(val !== '0'){
-          val+=n;
-          $('#number').val(val);
-      } else {
-          $('#number').val(n);
-      }
-    };
-
-    $('#btn1').click(function() {
-        addNumber(1);
-    });
-
-    $('#btn2').click(function() {
-        addNumber(2);
-    });
-
-    $('#btn3').click(function() {
-        addNumber(3);
-    });
-
-    $('#btn4').click(function() {
-        addNumber(4);
-    });
-
-    $('#btn5').click(function() {
-        addNumber(5);
-    });
-
-    $('#btn6').click(function() {
-        addNumber(6);
-    });
-
-    $('#btn7').click(function() {
-        addNumber(7);
-    });
-
-    $('#btn8').click(function() {
-        addNumber(8);
-    });
-
-    $('#btn9').click(function() {
-        addNumber(9);
-    });
-
-    $('#btn0').click(function() {
-        addNumber(0);
-    });
-
-
-
 
     $('#divisionBtn').on('click', function() {
         inDiv = true;
@@ -107,6 +48,7 @@ $(function() {
                 n1 = parseFloat($('#number').val());
                 $.session.set('value', n1);
                 startDivision = false;
+                enterSomeNumbers = true;
             } else {
                 n1 = parseFloat($('#number').val());
                 var tot = division(parseFloat($.session.get('value')),
@@ -118,8 +60,99 @@ $(function() {
         } else {
             equal = false;
             inDiv = true;
+            enterSomeNumbers = true;
         }
     });
+
+    $('#cleanLocal').click(function() {
+        $('#number').val('0');
+        $.session.set('value', 0);
+        startDivision = true, inSum = false,
+                inDiv = false, equal = false;
+
+    });
+
+    $('#btn1').click(function() {
+        if (enterSomeNumbers) {
+            cleanInput();
+            enterSomeNumbers = false;
+        }
+        addNumber(1);
+    });
+
+    $('#btn2').click(function() {
+        if (enterSomeNumbers) {
+            cleanInput();
+            enterSomeNumbers = false;
+        }
+        addNumber(2);
+    });
+
+    $('#btn3').click(function() {
+        if (enterSomeNumbers) {
+            cleanInput();
+            enterSomeNumbers = false;
+        }
+        addNumber(3);
+    });
+
+    $('#btn4').click(function() {
+        if (enterSomeNumbers) {
+            cleanInput();
+            enterSomeNumbers = false;
+        }
+        addNumber(4);
+    });
+
+    $('#btn5').click(function() {
+        if (enterSomeNumbers) {
+            cleanInput();
+            enterSomeNumbers = false;
+        }
+        addNumber(5);
+    });
+
+    $('#btn6').click(function() {
+        if (enterSomeNumbers) {
+            cleanInput();
+            enterSomeNumbers = false;
+        }
+        addNumber(6);
+    });
+
+    $('#btn7').click(function() {
+        if (enterSomeNumbers) {
+            cleanInput();
+            enterSomeNumbers = false;
+        }
+        addNumber(7);
+    });
+
+    $('#btn8').click(function() {
+        if (enterSomeNumbers) {
+            cleanInput();
+            enterSomeNumbers = false;
+        }
+        addNumber(8);
+    });
+
+    $('#btn9').click(function() {
+        if (enterSomeNumbers) {
+            cleanInput();
+            enterSomeNumbers = false;
+        }
+        addNumber(9);
+    });
+
+    $('#btn0').click(function() {
+        if (enterSomeNumbers) {
+            cleanInput();
+            enterSomeNumbers = false;
+        }
+        addNumber(0);
+    });
+
+
 
     $('#equalBtn').click(function() {
         equal = true;
@@ -148,12 +181,36 @@ $(function() {
             url: 'http://localhost/Practica1/public/greeting/people',
             data: o,
             success: function() {
-                alert('Ok');
+                //alert('Ok');
             },
             failure: function() {
-                alert("Fail!");
+                // alert("Fail!");
             }
 
         });
     });
+
+    var addNumber = function(n) {
+        var val = $('#number').val();
+        if (val !== '0') {
+            val += n;
+            $('#number').val(val);
+        } else {
+            $('#number').val(n);
+        }
+    };
+
+    var cleanInput = function() {
+        $('#number').val('');
+    };
+
+    var add = function(a, b) {
+        return a + b;
+
+    };
+
+    var division = function(a, b) {
+        return a / b;
+    };
+
 });
